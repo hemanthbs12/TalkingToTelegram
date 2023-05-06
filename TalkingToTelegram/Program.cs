7b1ffaa487eaeb3149c335c9276b55d1d3e99c70
Hemanth <hemanthbs12@hotmail.com>
2023-05-07 01:12:40 +0530

    Add project files.

---
+++ TalkingToTelegram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+
+namespace TalkingToTelegram
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var botClient = new TelegramBotClient("6********6:Aqwerty*************sdkn35r");
+                var chatId = "@te************h";
+                var message = "Hello, world!";
+                await botClient.SendTextMessageAsync(chatId, message, ParseMode.Markdown);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
